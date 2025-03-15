3a851dbbf7d9b95998679647fceb0439620ce52e
david <dpoulin45@gmail.com>
2025-03-15 01:10:13 +0100

    add database

--- index.php
+++ index.php
@@ -6,12 +6,27 @@ $f3 = \Base::instance();
 $f3->DEBUG = 3;
 $f3->PACKAGE ="wood51";
 
+$f3->set("DEBUG",3);
+//$f3->set('CACHE', true);
+// Configurer la base de données MariaDB
+try {
+    $db_name = getenv('MARIADB_DATABASE');
+    $db_user = getenv('MARIADB_USER');
+    $db_password = getenv('MARIADB_PASSWORD');
+    $db_host = getenv('MARIADB_HOST');
+
+    $db = new \DB\SQL("mysql:host=$db_host;port=3306;dbname=$db_name", $db_user, $db_password);
+    $f3->set('DB', $db);
+} catch (PDOException $e) {
+    die("Erreur de connexion à MariaDB : " . $e->getMessage());
+}
+
 $f3->AUTOLOAD = "app/services/";
 $f3->UI ="app/views";
+$f3->TZ ="Europe/Paris";
 
 $modules = \ModulesCore::instance();
-$modules->load("auth");
+//  $modules->load("auth");
 AnnotationRoutingPlugin::instance();
-
 
 $f3->run();
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '567a0c62a89dfdbfbea3092c1b8d7a0ca0b52a84',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '567a0c62a89dfdbfbea3092c1b8d7a0ca0b52a84',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
